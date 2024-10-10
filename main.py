22f51829fca95cc7692a2f39704c0dd4ea907a86
Gwizee <risingfire69@gmail.com>
2024-10-10 21:33:08 +0600

    Not Equal Activity has been completed

---
+++ main.py
@@ -0,0 +1,23 @@
+a = "Python"
+b = "Codingal"
+
+if a == b :
+    print("Values are same")
+else :
+    print("Values are not same")
+
+c = "HTML"
+d = "CSS"
+
+if c != d :
+    print("Values are not same")
+else :
+    print("Values are same")
+
+e = 2
+f = 3
+
+if (e==1) != (f==3) :
+    print("I am here")
+else :
+    print("I am not here")
